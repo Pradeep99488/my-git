c9a7efe0a1eb3347ca427798eac30c44d621e883
Pradeep99488 <87130746+Pradeep99488@users.noreply.github.com>
2023-02-13 01:54:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-var headerTitle=document.getElementById("header-title");
-document.getElementsByClassName("title").style.fontWeight="bold"
-document.getElementsByClassName("title").style.color='green'
+//var headerTitle=document.getElementById("header-title");
+//document.getElementsByClassName("title").style.fontWeight="bold"
+//document.getElementsByClassName("title").style.color='green'
+
+
+var items=document.getElementsByClassName("list-group-item");
+console.log(items)
+console.log(items[1]);
+items[1].textContent=" hello 2";
+items[1].getElementsByClassName.fontWeight="bold";
+items.style.backgroundColor="yellow";
+
+
+for(var i=0; i<itemslength;i++){
+    items[1].style.backgroundColor="#f4f4f4"
+}
+items[2].style.backgroundColor="green";
+items.style.fontWeight="bold"
