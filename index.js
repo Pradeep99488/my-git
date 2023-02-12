052adfd0e69e662828d765dbaafa1f02a469cdea
Pradeep99488 <87130746+Pradeep99488@users.noreply.github.com>
2023-02-13 01:41:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+var headerTitle=document.getElementById("header-title");
+document.getElementsByClassName("title").style.fontWeight="bold"
+document.getElementsByClassName("title").style.color='green'
